b31102027c8471d941f64b53e307b8da910e0b68
Marina Marcet <mmarcet@crg.es>
2020-08-16 16:23:58 +0200

    Content based item2item

--- content_based/content_based.py
+++ content_based/content_based.py
@@ -12,7 +12,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import argparse
 
-def get_recommendation(fic):
+def get_recommendation(fic,mapping_reduced,similarity_matrix,df_reduced):
     if fic in mapping_reduced:
         fic_index = mapping_reduced[fic]
     else:
@@ -23,7 +23,7 @@ def get_recommendation(fic):
     similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
     similarity_score = similarity_score[1:args.numR]
     fic_indices = [i[0] for i in similarity_score]
-    return (df_reduced[["idName","title","metadata"]].iloc[fic_indices],similarity_score)
+    return (df_reduced["idName"].iloc[fic_indices].tolist(),similarity_score)
 
 def find_optimal_clusters(data, max_k):
     iters = range(6, max_k+1, 2)
@@ -42,10 +42,53 @@ def find_optimal_clusters(data, max_k):
         ax.set_title('SSE by Cluster Center Plot - random_seed = '+str(a))
         plt.show()
 
+def reduce_through_clustering(word_matrix,df,ficName):
+    mapping = pd.Series(df.index,index = df["idName"])
+    fic_index = mapping[ficName]
+    cl = df.iloc[fic_index]["cluster"]
+    cluster_idx = df[df["cluster"] == cl].index
+    df_reduced = df[df["cluster"] == cl]
+    df_reduced.reset_index(drop=True,inplace=True)
+    word_matrix_reduced = word_matrix[cluster_idx]
+    return df,df_reduced, word_matrix_reduced,cluster_idx
+
+def reduce_through_wordNumber(word_matrix,df):
+    df_reduced = df.sort_values(by=["numWords"],ascending=False)[:50000]
+    numWords_idx = df_reduced.index
+    df_reduced.reset_index(drop=True,inplace=True)
+    word_matrix_reduced = word_matrix[numWords_idx]
+    return df_reduced, word_matrix_reduced
+
+def get_word_matrix(metadata,word_method,number_words):
+    if word_method == "tfidf":    
+        tfidf = TfidfVectorizer(stop_words="english",max_features=number_words,max_df=0.95) 
+        word_matrix = tfidf.fit_transform(metadata)
+    else:
+        count_vec = CountVectorizer(stop_words="english",max_features=number_words,max_df=0.95)
+        word_matrix = count_vec.fit_transform(metadata)
+    return word_matrix
+
+def get_similarity_matrix(word_matrix):
+    if args.word_method == "tfidf":
+        similarity_matrix = linear_kernel(word_matrix,word_matrix,dense_output=False)
+    else:
+        similarity_matrix = cosine_similarity(word_matrix,word_matrix,dense_output=False)
+    return similarity_matrix
+
+def get_clusters(word_matrix,df):
+    if args.optimize_clusters:
+        find_optimal_clusters(word_matrix, 20)
+        exit()
+    clusters = MiniBatchKMeans(n_clusters=args.numCl, init_size=1024, batch_size=2048, random_state=args.numR).fit_predict(word_matrix)
+    df["cluster"] = clusters
+    return df
+    
 parser = argparse.ArgumentParser(description="Content based recommender")
 parser.add_argument("-i",dest="metadataFile",action="store",required=True,help="File containing the fics metadata")
-parser.add_argument("-m",dest="filtering_mode",action="store",choices=["clustering","number_words"],default="number_words",help="How data will be filtered before prediction")
-parser.add_argument("-f",dest="ficInterest",action="store",type=int,required=True,help="Id of the fic you want to search similarities to")
+parser.add_argument("-m",dest="filtering_mode",action="store",choices=["clustering","number_words","clustering_by_characters","clustering_by_relationships"],default="number_words",help="How data will be filtered before prediction")
+parser.add_argument("-f",dest="ficInterest",action="store",type=int,default=None,help="Id of the fic you want to search similarities to, if none is provided the script will compute recommendations for all users")
+parser.add_argument("-w",dest="word_method",action="store",choices=["tfidf","counts"],default="tfidf",help="Method used to create the word matrix")
+parser.add_argument("-o",dest="outfileName",action="store",default="results.item2item.contentbased.txt",help="Output for all the recommendations")
 parser.add_argument("--number_clusters",dest="numCl",action="store",type=int,default=12,help="Number of clusters used in kmeans when clustering filter is selected")
 parser.add_argument("--random_seed",dest="randS",action="store",type=int,default=33,help="Random seed to run kmeans")
 parser.add_argument("--number_words",dest="numW",action="store",type=int,default=10000,help="Number of words in Tfid analysis")
@@ -56,44 +99,71 @@ parser.add_argument("--find_optimal_clusters",dest="optimize_clusters",action="s
 args = parser.parse_args()
     
 df = pd.read_csv(args.metadataFile,sep="\t",na_values="-")
-df["additional_tags"] = df["additional_tags"].fillna("")
-df["characters"] = df["characters"].fillna("")
-df["relationships"] = df["relationships"].fillna("")
+
+df = df.fillna("")
 df["metadata"] = df["additional_tags"]
+
 if args.addC:
     df["metadata"] = df["metadata"] + df["characters"]
 if args.addR:
     df["metadata"] = df["metadata"] + df["relationships"]
-    
-tfidf = TfidfVectorizer(stop_words="english",max_features=args.numW) 
-word_matrix = tfidf.fit_transform(df["metadata"])
+
+word_matrix = get_word_matrix(df["metadata"],args.word_method,args.numW)
+
 ficName = args.ficInterest
 
 if args.filtering_mode == "clustering":
-    if args.optimize_clusters:
-        find_optimal_clusters(word_matrix, 20)
-    clusters = MiniBatchKMeans(n_clusters=args.numCl, init_size=1024, batch_size=2048, random_state=33).fit_predict(word_matrix)
-    df["cluster"] = clusters
-    mapping = pd.Series(df.index,index = df["idName"])
-    fic_index = mapping[ficName]
-    cl = df.iloc[fic_index]["cluster"]
-    cluster_idx = df[df["cluster"] == cl].index
-    df_reduced = df[df["cluster"] == cl]
-    df_reduced.reset_index(drop=True,inplace=True)
-    word_matrix_reduced = word_matrix[cluster_idx]
-
+    df = get_clusters(word_matrix,df)
+    if ficName:
+        df, df_reduced, word_matrix_reduced = reduce_through_clustering(word_matrix,df,ficName)
+elif args.filtering_mode == "clustering_by_characters":
+    word_matrix1 = get_word_matrix(df["characters"],args.word_method,args.numW)
+    df = get_clusters(word_matrix1,df)
+    if ficName:
+        df, df_reduced, word_matrix1_reduced,cluster_idx = reduce_through_clustering(word_matrix1,df,ficName)
+        word_matrix_reduced = word_matrix[cluster_idx]
+elif args.filtering_mode == "clustering_by_relationships":
+    word_matrix1 = get_word_matrix(df["relationships"],args.word_method,args.numW)
+    df = get_clusters(word_matrix1,df)
+    if ficName:
+        df, df_reduced, word_matrix1_reduced,cluster_idx = reduce_through_clustering(word_matrix1,df,ficName)
+        word_matrix_reduced = word_matrix[cluster_idx]
 elif args.filtering_mode == "number_words":
-    df_reduced = df.sort_values(by=["numWords"],ascending=False)[:50000]
-    numWords_idx = df_reduced.index
-    df_reduced.reset_index(drop=True,inplace=True)
-    word_matrix_reduced = word_matrix[numWords_idx]
+    df_reduced, word_matrix_reduced = reduce_through_wordNumber(word_matrix,df)
 
-mapping_reduced = pd.Series(df_reduced.index,index = df_reduced["idName"])
-fic_index = mapping_reduced[ficName]
-similarity_matrix = linear_kernel(word_matrix_reduced,word_matrix_reduced,dense_output=False)
+if ficName:
+    mapping_reduced = pd.Series(df_reduced.index,index = df_reduced["idName"])
+    fic_index = mapping_reduced[ficName]
+    similarity_matrix = get_similarity_matrix(word_matrix_reduced)
 
-recom,similarity_score = get_recommendation(ficName)
+    recom,similarity_score = get_recommendation(ficName,mapping_reduced,similarity_matrix,df_reduced)
 
-print(df_reduced[df_reduced["idName"] == ficName][["idName","title","metadata"]])
-print(recom)
-print(similarity_score)
+    print(df_reduced[df_reduced["idName"] == ficName][["idName","title","metadata"]])
+    print(recom)
+    print(similarity_score)
+else:
+    print("Warning: All recommendations will be computed")
+    if args.filtering_mode == "number_words":
+        mapping_reduced = pd.Series(df_reduced.index,index = df_reduced["idName"])
+        similarity_matrix = get_similarity_matrix(word_matrix_reduced)
+        with open(args.outfileName,"w") as outfile:
+            for a in range(50000):
+                ficName = df_reduced.iloc[a]["idName"]
+                recom, similarity_score = get_recommendation(ficName,mapping_reduced,similarity_matrix,df_reduced)
+                recom = [str(x) for x in recom]
+                print(str(ficName)+"\t"+";".join(recom),file=outfile)
+    else:
+        with open(args.outfileName,"w") as outfile:
+            for cl in range(args.numCl):
+                cluster_idx = df[df["cluster"] == cl].index
+                df_reduced = df[df["cluster"] == cl]
+                df_reduced.reset_index(drop=True,inplace=True)
+                word_matrix_reduced = word_matrix[cluster_idx]
+                mapping_reduced = pd.Series(df_reduced.index,index = df_reduced["idName"])
+                similarity_matrix = get_similarity_matrix(word_matrix_reduced)
+                for a in range(df_reduced.shape[0]):
+                    ficName = df_reduced.iloc[a]["idName"]
+                    recom, similarity_score = get_recommendation(ficName,mapping_reduced,similarity_matrix,df_reduced)
+                    recom = [str(x) for x in recom]
+                    print(str(cl)+"\t"+str(ficName)+"\t"+";".join(recom),file=outfile)
+            
--- scrapper/clean_retrieved_data.py
+++ scrapper/clean_retrieved_data.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.sparse as sparse
+
 #Clean metadata obtained from the AO3 scrapper
 
-with open("metadata_fics.cleaned.txt","w") as outfile:
+inputFile = "metadata_fics.txt"
+outfileCleaned = "metadata_fics.cleaned.txt"
+user_to_item_table_content = "user_to_item.content.txt"
+user_to_item_table_collab = "user_to_item.collab.txt"
+
+info = {}
+ficInfo = {}
+with open(outfileCleaned,"w") as outfile:
     #Print header
     header = [x.strip() for x in open("header.txt")][0]
     idNames = set([])
     print(header,file=outfile)
-    with open("metadata_fics.txt","r") as infile:
+    with open(inputFile,"r") as infile:
         for line in infile:
             line = line.strip()
             dades = line.split("\t")
-            if dades[0] not in idNames:
-                idNames.add(dades[0])
+            ficId = dades[0]
+            #Remove duplicates if present
+            if ficId not in idNames:
+                idNames.add(ficId)
+                #Fill empty fields with - it will be our NA value
                 num_unknown_fields = dades.count("-")
                 if num_unknown_fields > 3:
                     pass
                 else:
+                    #Save information for the user to item matrix
+                    author = dades[1]
+                    name = dades[2]
+                    ficInfo[ficId] = [name,author,dades[3],dades[6],dades[11]]
+                    if dades[-1] != "-":
+                        readers = dades[-1].split("|")
+                    else:
+                        readers = None
+                    if author not in info:
+                        info[author] = {}
+                    info[author][ficId] = "2.0"
+                    if readers:
+                        for r in readers:
+                            if r not in info:
+                                info[r] = {}
+                            info[r][ficId] = "1.0"
                     #Replace , in numbers
                     dades[6] = dades[6].replace(",","")
                     try:
@@ -35,3 +65,69 @@ with open("metadata_fics.cleaned.txt","w") as outfile:
                     dades[12] = dades[12].replace(" ","").replace("|"," ")
                     dades[13] = dades[13].replace(" ","").replace("|"," ")
                     print("\t".join(dades),file=outfile)
+
+
+mappingItems = {}
+mappingUsers = {}
+
+all_users = list(info.keys())
+#Marks users that have liked less than 10 fics
+users_cold_start = set([x for x in all_users if len(info[x]) < 10])
+
+#Marks fics that have been liked less than 10 times
+ficCounter = {}
+for userId, fics in info.items():
+    for fic in fics:
+        if fic not in ficCounter:
+            ficCounter[fic] = set([])
+        ficCounter[fic].add(userId)
+fics_cold_start = set([x for x in ficCounter if len(ficCounter[x]) < 10])
+
+#Print the user to item relation that will be used for collaborative analyses
+with open(user_to_item_table_collab,"w") as outTableCollab:
+    for userId,fics in info.items():
+        if userId not in users_cold_start:
+            mappingUsers[userId] = str(len(mappingUsers))
+            for fic in fics:
+                if fic not in fics_cold_start:
+                    if fic not in mappingItems:
+                        mappingItems[fic] = str(len(mappingItems))
+                    print(mappingUsers[userId]+"\t"+mappingItems[fic]+"\t"+info[userId][fic],file=outTableCollab)
+
+#Assign indexes to cold start users and fics
+for userId in users_cold_start:
+    mappingUsers[userId] = str(len(mappingUsers))
+
+for ficId in fics_cold_start:
+    mappingItems[ficId] = str(len(mappingItems))
+
+#Print user to item relations that will be used for content based analyses
+
+only_read_by_cold_users = set([])
+with open(user_to_item_table_content, "w") as outTable:
+    for userId,fics in info.items():
+        for fic in fics:
+            if fic not in mappingItems:
+                mappingItems[fic] = str(len(mappingItems))
+                only_read_by_cold_users.add(fic)
+            # ~ print(userId,fic,info[userId][fic])
+            print(mappingUsers[userId]+"\t"+mappingItems[fic]+"\t"+info[userId][fic],file=outTable)
+
+#Print to file the code conversions for users and fics so that was can get them back
+with open("mappingUsers.txt","w") as outUsers:
+    for code in mappingUsers:
+        if code in users_cold_start:
+            tag = "cold"
+        else:
+            tag = "fine"
+        print(code+"\t"+mappingUsers[code]+"\t"+tag,file=outUsers)
+
+with open("mappingItems.txt", "w") as outFics:
+    for code in mappingItems:
+        if code in fics_cold_start:
+            tag = "cold"
+        elif tag in only_read_by_cold_users:
+            tag = "read_by_col_users"
+        else:
+            tag = "fine"
+        print(code+"\t"+mappingItems[code]+"\t"+tag,file=outFics)
